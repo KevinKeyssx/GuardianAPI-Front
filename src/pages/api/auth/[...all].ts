784e4eda4f5961c4cb4df4ae1c6c0b23a070e353
kevinkeyssx <keyss1700@gmail.com>
2025-04-16 00:01:28 -0400

    server better-auth

---
+++ src/pages/api/auth/[...all].ts
@@ -0,0 +1,11 @@
+import { auth } from "../../../lib/utils/auth";
+
+import type { APIRoute } from "astro";
+
+
+export const prerender = false;
+
+
+export const ALL: APIRoute = async (ctx) => {
+	return auth.handler(ctx.request);
+};
