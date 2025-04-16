ae60fe67347baf417fb7fc44ed6c3694db65145f
kevinkeyssx <keyss1700@gmail.com>
2025-04-16 00:19:46 -0400

    Env format

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,16 +1,15 @@
 import { z } from "zod";
 
 const envVariables = z.object({
-    DATABASE_URL: z.string().url(),
-    PORT: z.number().default(3000),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
-    BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string().url(),
+    GITHUB_CLIENT_ID        : z.string(),
+    GITHUB_CLIENT_SECRET    : z.string(),
+    BETTER_AUTH_SECRET      : z.string(),
+    BETTER_AUTH_URL         : z.string().url(),
 });
 
+
 const _env = envVariables.safeParse(process.env);
+
 
 if (_env.success === false) {
     console.error(
