585759035de85b1404cd90260c42c77b72560ea3
kevinkeyssx <keyss1700@gmail.com>
2025-04-17 05:31:34 -0400

    Test middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,37 @@
-import { auth } from "./src/lib/utils/auth";
+// import { auth } from "./src/lib/utils/auth";
 
-import { defineMiddleware } from "astro:middleware";
+// import { defineMiddleware } from "astro:middleware";
 
+
+// export const onRequest = defineMiddleware(async (context, next) => {
+//     const isAuthed = await auth.api
+//         .getSession({
+//             headers: context.request.headers,
+//         })
+
+//     if (isAuthed) {
+//         context.locals.user = isAuthed.user;
+//         context.locals.session = isAuthed.session;
+//     } else {
+//         context.locals.user = null;
+//         context.locals.session = null;
+//     }
+
+//     return next();
+// });
+
+
+import { auth } from './lib/utils/auth';
+import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    const isAuthed = await auth.api
-        .getSession({
-            headers: context.request.headers,
-        })
+    const session = await auth.api.getSession({
+        headers: context.request.headers
+    });
+    console.log('🚀 ~ file: middleware.ts:29 ~ session:', session)
 
-    if (isAuthed) {
-        context.locals.user = isAuthed.user;
-        context.locals.session = isAuthed.session;
-    } else {
-        context.locals.user = null;
-        context.locals.session = null;
-    }
+    context.locals.session = session?.session ?? null;
+    context.locals.user    = session?.user    ?? null;
 
     return next();
 });
