46aca36fa16d06b9fb926a840c0daaeb0cab84b2
kevinkeyssx <keyss1700@gmail.com>
2025-04-17 05:28:15 -0400

    Styles in global

---
+++ src/components/buttons/NeonButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let onClick: () => void;
+</script>
+
+<button
+    class       = "neon_button hover:scale-105 rounded-lg font-semibold flex items-center gap-2 justify-center"
+    on:click    = { onClick }
+>
+    <slot/>
+</button>
