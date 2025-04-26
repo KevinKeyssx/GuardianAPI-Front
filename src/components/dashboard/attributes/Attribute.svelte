1f3d78936f50b97eefda932fc564d39bfcc0937d
kevinkeyssx <keyss1700@gmail.com>
2025-04-26 05:36:47 -0400

    Attribute svelte

---
+++ src/components/dashboard/attributes/Attribute.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import Panel from "@/components/shared/Panel.svelte";
+    import AttributeForm from "./AttributeForm.svelte";
+
+    let clicked = 0;
+
+    export let columns: string[];
+    console.log('🚀 ~ file: Attribute.svelte:8 ~ columns:', columns)
+</script>
+
+<Panel
+    id          = "add-attribute"
+    title       = "Add Attribute"
+    buttonText  = "Add Attribute"
+    isEdit      = { false }
+    buttonClass = "fixed sm:static sm:flex bottom-16 sm:bottom-auto right-4 sm:right-auto bg-neon-blue text-dark-blue rounded-full sm:rounded-lg px-2.5 sm:px-4 sm:py-2 hover:bg-opacity-80 transition-colors duration-300 sm:items-center "
+    bind:clicked={ clicked }
+>
+    <AttributeForm
+        key={'llave'}
+        bind:clicked={ clicked }
+    />
+</Panel>
