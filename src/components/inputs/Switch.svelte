8979f0d5551e7114fc3ee1c7dce6871e34c96ff5
kevinkeyssx <keyss1700@gmail.com>
2025-05-01 03:23:43 -0400

    Checked & onCheckedChange added

--- src/components/inputs/Switch.svelte
+++ src/components/inputs/Switch.svelte
@@ -4,6 +4,11 @@
     export let label    = "Do not disturb";
     export let id       = "dnd";
     export let name     = "hello";
+    export let checked  = false;
+    
+    function handleChange(value: boolean) {
+        checked = value;
+    }
 </script>
 
 <div class="flex items-center justify-between py-2">
@@ -12,6 +17,8 @@
     <Switch.Root
         {id}
         {name}
+        checked={checked}
+        onCheckedChange={handleChange}
         class="focus-visible:ring-neon-blue focus-visible:ring-offset-space-blue data-[state=checked]:bg-neon-blue data-[state=unchecked]:bg-space-blue data-[state=unchecked]:border-neon-blue/50 focus-visible:outline-none peer inline-flex h-[28px] min-h-[28px] w-[52px] shrink-0 cursor-pointer items-center rounded-full border border-neon-blue/50 px-[2px] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
     >
         <Switch.Thumb
