4f23ef4ea4685febb9ff054b80af2201eb9cd35d
kevinkeyssx <keyss1700@gmail.com>
2025-05-01 03:31:38 -0400

    Query User added

---
+++ src/lib/graphql/users/queries.ts
@@ -0,0 +1,26 @@
+import { gql } from 'graphql-request';
+
+export const USERS_QUERY = gql`
+    query Users($page: Int, $each: Int, $field: String, $orderBy: String, $attributeKeys: [String!]) {
+        users(page: $page, each: $each, field: $field, orderBy: $orderBy) {
+            id
+            avatar
+            email
+            name
+            nickname
+            birthdate
+            phone
+            isActive
+            createdAt
+            updatedAt
+            roles {
+                name
+            }
+            attributes(keys: $attributeKeys) {
+                key
+                value
+            }
+            total
+        }
+    }
+`;
