2c9173f38db16360f0637781ffb2a4556c85e2cc
kevinkeyssx <keyss1700@gmail.com>
2025-05-01 03:28:32 -0400

    Table .svelte remaned

---
+++ src/components/shared/table/Table.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import TableBody            from './TableBody.svelte';
+    import TableHead            from './TableHead.svelte';
+    import type { ColumnProp }  from './column.model';
+
+    export let columns: ColumnProp[];
+</script>
+
+<div class="bg-dark-blue/50 rounded-lg border border-neon-blue/20 overflow-auto w-full">
+    <table class="min-w-full divide-y divide-neon-blue/20">
+        <TableHead {columns} />
+
+        <TableBody>
+            <slot />
+        </TableBody>
+    </table>
+</div>
