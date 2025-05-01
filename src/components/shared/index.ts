2c2bd87d4d8c6e2eef0e5eb92f5723a684916357
kevinkeyssx <keyss1700@gmail.com>
2025-05-01 03:24:10 -0400

    Index of shared added

---
+++ src/components/shared/index.ts
@@ -0,0 +1,9 @@
+// Table
+export * from './table';
+
+// Dropdown
+export { default as Dropdown }      from './dropdown/Dropdown.astro';
+export { default as DropdownItem }  from './dropdown/DropdownItem.astro';
+
+// Popover
+export { default as Popover } from './popover/Popover.svelte';
