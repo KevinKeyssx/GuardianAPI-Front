ecda665392fadf425663479619b9cf1e1311de60
kevinkeyssx <keyss1700@gmail.com>
2025-05-01 03:28:01 -0400

    Index TAble added

---
+++ src/components/shared/table/index.ts
@@ -0,0 +1,7 @@
+export { default as Table }         from './Table.svelte';
+export { default as TableHead }     from './TableHead.svelte';
+export { default as TableRow }      from './TableRow.svelte';
+export { default as TableData }     from './TableData.svelte';
+export { default as TableBody }     from './TableBody.svelte';
+export { default as Pagination }    from './Pagination.svelte';
+export *                            from './column.model';
