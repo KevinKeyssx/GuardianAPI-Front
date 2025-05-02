58b04834be8b45e166ebd8b777ceab25356e8725
kevinkeyssx <keyss1700@gmail.com>
2025-05-02 06:00:11 -0400

    User Section Wrapper added

---
+++ src/components/dashboard/users/UserSectionWrapper.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { QueryClientProvider } from '@tanstack/svelte-query';
+
+    import UserSection      from '@/components/dashboard/users/UserSection.svelte';
+    import { queryClient }  from '@/lib/graphql/query-client';
+</script>
+
+<QueryClientProvider client={queryClient}>
+    <UserSection />
+</QueryClientProvider>
