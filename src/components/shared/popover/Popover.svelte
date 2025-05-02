c0a40e3159dbafab2871af8c7dd7c900f3055981
kevinkeyssx <keyss1700@gmail.com>
2025-05-02 06:01:40 -0400

    Animation styles implemented

--- src/components/shared/popover/Popover.svelte
+++ src/components/shared/popover/Popover.svelte
@@ -13,7 +13,7 @@
 
     <Popover.Portal>
         <Popover.Content
-            class="dropdown-content z-50 w-56 rounded-md shadow-lg bg-dark-blue border border-neon-blue/30 py-3 px-3"
+            class="mr-3 w-56 z-40 rounded-md py-3 px-3 bg-dark-blue/50 backdrop-blur-md border border-neon-blue/30 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
             sideOffset={8}
         >
             <slot />
