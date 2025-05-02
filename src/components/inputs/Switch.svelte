23fcf286ee00fcb5f0b9f7434c48f8162e01030d
kevinkeyssx <keyss1700@gmail.com>
2025-05-02 06:00:45 -0400

    Props added

--- src/components/inputs/Switch.svelte
+++ src/components/inputs/Switch.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
     import { Label, Switch } from "bits-ui";
 
-    export let label    = "Do not disturb";
-    export let id       = "dnd";
-    export let name     = "hello";
-    export let checked  = false;
-    
-    function handleChange(value: boolean) {
-        checked = value;
+
+    type Props = {
+        label?   : string;
+        id?      : string;
+        name?    : string;
+        checked? : boolean | undefined;
     }
+
+
+    let {
+        label    = "Switch",
+        id       = "switch",
+        name     = "switch",
+        checked  = $bindable<boolean | undefined>(),
+    }: Props = $props();
+
+
+    if ( checked === undefined ) {
+        checked = false
+    };
 </script>
 
-<div class="flex items-center justify-between py-2">
-    <Label.Root for={id} class="text-sm font-orbitron text-white">{label}</Label.Root>
+<div class="flex items-center justify-between">
+    <Label.Root
+        for     = { id }
+        class   = "text-sm font-orbitron text-white">
+        { label }
+    </Label.Root>
 
     <Switch.Root
-        {id}
-        {name}
-        checked={checked}
-        onCheckedChange={handleChange}
-        class="focus-visible:ring-neon-blue focus-visible:ring-offset-space-blue data-[state=checked]:bg-neon-blue data-[state=unchecked]:bg-space-blue data-[state=unchecked]:border-neon-blue/50 focus-visible:outline-none peer inline-flex h-[28px] min-h-[28px] w-[52px] shrink-0 cursor-pointer items-center rounded-full border border-neon-blue/50 px-[2px] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        bind:checked={checked}
+        { id }
+        { name }
+        onCheckedChange = {( value: boolean ) => checked = value }
+        class           = "focus-visible:ring-neon-blue focus-visible:ring-offset-space-blue data-[state=checked]:bg-neon-blue data-[state=unchecked]:bg-space-blue data-[state=unchecked]:border-neon-blue/50 focus-visible:outline-none peer inline-flex h-[28px] min-h-[28px] w-[52px] shrink-0 cursor-pointer items-center rounded-full border border-neon-blue/50 px-[2px] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
     >
         <Switch.Thumb
             class="bg-white data-[state=checked]:bg-dark-blue pointer-events-none block size-[22px] shrink-0 rounded-full shadow-md transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
