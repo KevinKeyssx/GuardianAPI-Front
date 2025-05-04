947949e65ba0e67e2d26be3a042bb433c496ae82
kevinkeyssx <keyss1700@gmail.com>
2025-05-04 02:37:16 -0400

    SERVER_ENV updated

--- auth.config.js
+++ auth.config.js
@@ -1,25 +1,26 @@
 import GitHub           from '@auth/core/providers/github'
 import { defineConfig } from 'auth-astro'
 
-import { ENV } from "./src/lib/utils/env";
+import { SERVER_ENV } from "@/lib/env/server";
 
 export default defineConfig({
 	providers: [
 		GitHub({
-			clientId: ENV.GITHUB_CLIENT_ID,
-			clientSecret: ENV.GITHUB_CLIENT_SECRET,
+			clientId        : SERVER_ENV.GITHUB_CLIENT_ID,
+			clientSecret    : SERVER_ENV.GITHUB_CLIENT_SECRET,
 		}),
 	],
-    secret: ENV.AUTH_SECRET,
-    trustHost: true,
-    cookies: {
+    secret      : SERVER_ENV.AUTH_SECRET,
+    trustHost   : true,
+    cookies     : {
         sessionToken: {
-            name: `next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: 'lax',
-                path: '/',
-                secure: true
+            name    : `astro-auth.session-token`,
+            options : {
+                httpOnly    : true,
+                sameSite    : 'lax',
+                path        : '/',
+                secure      : true,
+                maxAge      : 60 * 60 * 24 * 7,
             }
         }
     },
