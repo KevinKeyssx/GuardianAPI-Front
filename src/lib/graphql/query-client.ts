4ecb413e6780c471aa12ae944713553f9824ac2f
kevinkeyssx <keyss1700@gmail.com>
2025-05-04 02:38:43 -0400

    Client env implemented

--- src/lib/graphql/query-client.ts
+++ src/lib/graphql/query-client.ts
@@ -1,28 +1,32 @@
-import { ENV } from '@/lib/utils/env';
-import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-import { GraphQLClient } from 'graphql-request';
+import {
+    QueryClient,
+    QueryClientProvider
+}                           from '@tanstack/svelte-query';
+import { GraphQLClient }    from 'graphql-request';
+
+import { CLIENT_ENV } from '@/lib/env/client';
+
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            retry: 2, // Reintentar 2 veces en caso de error
-            staleTime: 1000 * 60 * 5, // 5 minutos de caché
-            refetchOnWindowFocus: false, // No recargar al enfocar la ventana
+            retry                   : 2, // Reintentar 2 veces en caso de error
+            staleTime               : 1000 * 60 * 5, // 5 minutos de caché
+            refetchOnWindowFocus    : false, // No recargar al enfocar la ventana
         },
     },
 });
 
 export const graphqlClient = new GraphQLClient(
-    // ENV.GUARDIANAPI_GRAPHQL_URL,
-    'http://localhost:3007/graphql',
+    CLIENT_ENV.PUBLIC_GUARDIANAPI_GRAPHQL_URL,
     {
         credentials: 'include',
 
         headers: () => ({
             'X-CSRF-Token': document.cookie
-                .split('; ')
-                .find((row) => row.startsWith('csrfToken='))
-                ?.split('=')[1] || '',
+                .split( '; ' )
+                .find(( row ) => row.startsWith( 'csrfToken=' ))
+                ?.split( '=' )[1] || '',
         }),
     }
 );
