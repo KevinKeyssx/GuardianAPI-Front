b7339efd3a8979ed4041766c66b22eef5414e62f
kevinkeyssx <keyss1700@gmail.com>
2025-05-05 18:47:24 -0400

    AttributeWrapper component added

---
+++ src/components/dashboard/attributes/AttributeWrapper.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { QueryClientProvider } from '@tanstack/svelte-query';
+
+    import { queryClient }      from '@/lib/graphql/query-client';
+    import AttributesSection    from './AttributesSection.svelte';
+</script>
+
+<QueryClientProvider client={queryClient}>
+    <AttributesSection />
+</QueryClientProvider>
