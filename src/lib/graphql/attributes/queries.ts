6e522ba8c44ac33738f06ef4245ddd82271e9df9
kevinkeyssx <keyss1700@gmail.com>
2025-05-06 02:01:37 -0400

    Values of attributes

--- src/lib/graphql/attributes/queries.ts
+++ src/lib/graphql/attributes/queries.ts
@@ -1,23 +1,20 @@
 import { gql } from 'graphql-request';
 
 export const ATTRIBUTES_QUERY = gql`
-    query Attributes($page: Int, $each: Int, $field: String, $orderBy: String, $attributeKeys: [String!]) {
-        attributes(page: $page, each: $each, field: $field, orderBy: $orderBy) {
-            id
+    query UserAttributes($keys: [String!]) {
+        userAttributes(keys: $keys) {
             key
-            value
+            defaultValue
             type
             required
             min
             max
-            minLenght
-            maxLenght
+            minLength
+            maxLength
             minDate
             maxDate
             pattern
-            active
-            createdAt
-            updatedAt
+            isActive
         }
     }
 `;
