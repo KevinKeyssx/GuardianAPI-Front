b37702a305dbaef9618adb601250ec7f28a39bdd
kevinkeyssx <keyss1700@gmail.com>
2025-05-07 18:13:04 -0400

    Role Wrapper component added

---
+++ src/components/dashboard/roles/RoleWrapper.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { QueryClientProvider } from '@tanstack/svelte-query';
+
+    import RoleSection      from './RoleSection.svelte';
+    import { queryClient }  from '@/lib/graphql/query-client';
+</script>
+
+<QueryClientProvider client={queryClient}>
+    <RoleSection />
+</QueryClientProvider>
