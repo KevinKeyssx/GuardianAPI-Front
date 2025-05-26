c524a9f7387e5ed1ac88433c9214267dc5c37844
kevinkeyssx <keyss1700@gmail.com>
2025-05-26 01:24:35 -0400

    Urql added

---
+++ src/lib/urql.ts
@@ -0,0 +1,19 @@
+import { Client, cacheExchange, fetchExchange } from '@urql/svelte';
+import { CLIENT_ENV } from '@/lib/env/client';
+
+
+export const client = new Client({
+    url: CLIENT_ENV.PUBLIC_GUARDIANAPI_GRAPHQL_URL,
+    exchanges: [cacheExchange, fetchExchange],
+    fetchOptions: () => {
+        return {
+            credentials: 'include',
+            headers: {
+                'X-CSRF-Token': document.cookie
+                    .split('; ')
+                    .find((row) => row.startsWith('csrfToken='))
+                    ?.split('=')[1] || '',
+            }
+        };
+    }
+});
