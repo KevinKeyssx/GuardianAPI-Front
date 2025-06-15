97c13ce07ac070de5e97fcd5bffbf9c8e27923f4
kevinkeyssx <keyss1700@gmail.com>
2025-06-14 23:49:18 -0400

    Order urql

--- src/lib/urql.ts
+++ src/lib/urql.ts
@@ -3,17 +3,15 @@ import { CLIENT_ENV } from '@/lib/env/client';
 
 
 export const client = new Client({
-    url: CLIENT_ENV.PUBLIC_GUARDIANAPI_GRAPHQL_URL,
-    exchanges: [cacheExchange, fetchExchange],
-    fetchOptions: () => {
-        return {
-            credentials: 'include',
-            headers: {
-                'X-CSRF-Token': document.cookie
-                    .split('; ')
-                    .find((row) => row.startsWith('csrfToken='))
-                    ?.split('=')[1] || '',
-            }
-        };
-    }
+    url             : CLIENT_ENV.PUBLIC_GUARDIANAPI_GRAPHQL_URL,
+    exchanges       : [cacheExchange, fetchExchange],
+    fetchOptions    : () => ({
+        credentials : 'include',
+        headers     : {
+            'X-CSRF-Token': document.cookie
+                .split( '; ' )
+                .find(( row ) => row.startsWith( 'csrfToken=' ))
+                ?.split( '=' )[1] || '',
+        }
+    })
 });
