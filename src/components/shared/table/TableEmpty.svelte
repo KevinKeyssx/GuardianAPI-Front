c990ad4eb268bc6d49a85b34b5f4fef70123f76d
kevinkeyssx <keyss1700@gmail.com>
2025-06-15 21:03:03 -0400

    Table Empty component added

---
+++ src/components/shared/table/TableEmpty.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import TableRow from "./TableRow.svelte";
+
+    export let columns: number;
+    export let data: string = 'No data found';
+</script>
+
+<TableRow>
+    <td colspan="{columns}" class="text-center h-16 text-white">
+        {data}
+    </td>
+</TableRow>
