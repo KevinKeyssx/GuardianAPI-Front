d1313e8cbf8dd8400aab3f8ccc68851584427f16
kevinkeyssx <keyss1700@gmail.com>
2025-06-21 22:50:07 -0400

    Type added

--- src/lib/graphql/attributes/types.ts
+++ src/lib/graphql/attributes/types.ts
@@ -1,19 +1,21 @@
+export type Type = 'NUMBER' | 'DECIMAL' | 'STRING' | 'DATETIME' | 'BOOLEAN' | 'JSON' | 'LIST' | 'UUID';
+
 export interface Attribute {
-    id?         : string;
-    key?        : string;
-    defaultValue?      : string;
-    type?       : string;
-    required?   : boolean;
-    min?        : string;
-    max?        : string;
-    minLength?  : number;
-    maxLength?  : number;
-    minDate?    : string;
-    maxDate?    : string;
-    pattern?    : string;
-    isActive?     : boolean;
-    createdAt?  : string;
-    updatedAt?  : string;
+    id?             : string;
+    key?            : string;
+    defaultValue?   : any;
+    type?           : Type;
+    required?       : boolean;
+    min?            : string;
+    max?            : string;
+    minLength?      : number;
+    maxLength?      : number;
+    minDate?        : string;
+    maxDate?        : string;
+    pattern?        : string;
+    isActive?       : boolean;
+    createdAt?      : string;
+    updatedAt?      : string;
 }
 
 export interface AttributesQuery {
