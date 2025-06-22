8b4d6bedf66e9ad706d1b58aa5ac8cd93ff183f2
kevinkeyssx <keyss1700@gmail.com>
2025-06-21 22:46:44 -0400

    Attribute Form complemeted updated

--- src/components/dashboard/attributes/AttributeForm.svelte
+++ src/components/dashboard/attributes/AttributeForm.svelte
@@ -1,118 +1,208 @@
 <script lang="ts">
+    import { PuzzlePiece } from "phosphor-svelte";
+
     import DatePicker   from "@/components/inputs/DatePicker.svelte";
     import Input        from "@/components/inputs/Input.svelte";
     import Select       from "@/components/inputs/Select.svelte";
     import Switch       from "@/components/inputs/Switch.svelte";
+    import Textarea     from "@/components/inputs/Textarea.svelte";
     import Modal        from "@/components/shared/Modal.svelte";
     import PanelFooter  from "@/components/shared/panel/PanelFooter.svelte";
     import PanelMain    from "@/components/shared/panel/PanelMain.svelte";
+    import DateForm     from "@/components/shared/show/date-form.svelte";
+
+    import type { Attribute } from "@/lib/graphql/attributes/types";
 
 
-    // type Exps = {
-        // key?: string;
-        // required?: boolean;
-        // isPanelOpen?: boolean;
-    // }
+    type Props = {
+        attribute: Attribute;
+        clicked: number;
+    }
 
 
-    // let { key = '', required, isPanelOpen = $bindable<boolean>(false) } = $props() as Exps;
+    let {
+        attribute: incomingAttribute,
+        clicked = $bindable(),
+    }: Props = $props();
 
-    export let key : string;
-    export let clicked : number;
+
+    let attribute = $state<Attribute>( incomingAttribute );
+
+
+    const options = [
+        { label: 'String',      value: 'STRING' },
+        { label: 'Number',      value: 'NUMBER' },
+        { label: 'Boolean',     value: 'BOOLEAN' },
+        { label: 'Decimal',     value: 'DECIMAL' },
+        { label: 'List',        value: 'LIST' },
+        { label: 'Json',        value: 'JSON' },
+        { label: 'Datetime',    value: 'DATETIME' },
+        { label: 'UUID',        value: 'UUID' },
+    ];
 </script>
 
 <PanelMain>
+    <form class="space-y-4">
+        <Input
+            bind:value  = { attribute.key }
+            label       = "Key Name"
+            placeholder = "Enter key name"
+            id          = "key"
+            name        = "key"
+            type        = 'text'
+        />
 
-<form class="space-y-4">
-    <Input
-        label       = "Key Name"
-        placeholder = "Enter key name"
-        id          = "key"
-        name        = "key"
-        type        = 'text'
-        bind:value  = {key}
+        <Select
+            bind:value  = { attribute.type }
+            onChange    = { () => attribute.defaultValue = null }  
+            label       = "Type"
+            placeholder = "Select type"
+            id          = "type"
+            name        = "type"
+            { options }
+        >
+            {#snippet icon()}
+                <PuzzlePiece class="size-5" />
+            {/snippet}
+        </Select>
+
+        {#if attribute.type === 'NUMBER' || attribute.type === 'DECIMAL'}
+            <Input
+                bind:value  = { attribute.defaultValue }
+                label       = "Default Value"
+                placeholder = "Enter default value"
+                id          = "default-value"
+                name        = "default-value"
+                type        = 'number'
+            />
+        {:else if attribute.type === 'STRING' || attribute.type === 'UUID'}
+            <Input
+                bind:value  = { attribute.defaultValue }
+                label       = "Default Value"
+                placeholder = "Enter default value"
+                id          = "default-value"
+                name        = "default-value"
+                type        = 'text'
+            />
+        {:else if attribute.type === 'DATETIME'}
+            <DatePicker
+                bind:value    = { attribute.defaultValue }
+                id            = "default-value"
+                label         = "Default Value"
+            />
+        {:else if attribute.type === 'BOOLEAN'}
+            <Switch
+                bind:checked    = { attribute.defaultValue }
+                label           = "Default Value"
+                id              = "default-value"
+            />
+        {:else if attribute.type === 'JSON'}
+            <Textarea
+                bind:value  = { attribute.defaultValue }
+                label       = "Default Value"
+                placeholder = "Enter default value"
+                id          = "default-value"
+                name        = "default-value"
+            />
+        {:else if attribute.type === 'LIST'}
+            <Input
+                bind:value  = { attribute.defaultValue }
+                label       = "Default Value"
+                placeholder = "Enter default value"
+                id          = "default-value"
+                name        = "default-value"
+                type        = 'text'
+            />
+        {/if}
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
+            {#if attribute.type === 'NUMBER' || attribute.type === 'DECIMAL'}
+                <Input
+                    bind:value  = { attribute.min }
+                    label       = "Min Value"
+                    placeholder = "Enter min value"
+                    id          = "min-value"
+                    name        = "min-value"
+                    type        = 'number'
+                />
+
+                <Input
+                    bind:value  = { attribute.max }
+                    label       = "Max Value"
+                    placeholder = "Enter max value"
+                    id          = "max-value"
+                    name        = "max-value"
+                    type        = 'number'
+                />
+            {:else if attribute.type === 'STRING'}
+                <Input
+                    bind:value  = { attribute.minLength }
+                    label       = "Min Length Value"
+                    placeholder = "Enter min Length"
+                    id          = "min-value-length"
+                    name        = "min-value-length"
+                    type        = 'number'
+                />
+
+                <Input
+                    bind:value  = { attribute.maxLength }
+                    label       = "Max Length Value"
+                    placeholder = "Enter max Length"
+                    id          = "max-value-length"
+                    name        = "max-value-length"
+                    type        = 'number'
+                />
+
+                <div class=" grid col-span-2">
+                    <Input
+                        bind:value  = { attribute.pattern }
+                        label       = "Pattern"
+                        placeholder = "Enter Pattern"
+                        id          = "pattern"
+                        name        = "pattern"
+                        type        = 'text'
+                    />
+                </div>
+            {:else if attribute.type === 'DATETIME'}
+                <DatePicker
+                    bind:value    = { attribute.minDate }
+                    id            = "min-date"
+                    label         = "Min Date"
+                />
+
+                <DatePicker
+                    bind:value    = { attribute.maxDate }
+                    id            = "max-date"
+                    label         = "Max Date"
+                />
+            {/if}
+        </div>
+
+        <Switch
+            bind:checked    = { attribute.required }
+            label           = "Required"
+            id              = "required"
+        />
+    </form>
+
+    <DateForm
+        show        = { !!attribute.id }
+        createdAt   = { attribute.createdAt }
+        updatedAt   = { attribute.updatedAt }
     />
-
-    <Select />
-
-    <Input
-        label       = "Default Value"
-        placeholder = "Enter default value"
-        id          = "dvalue"
-        name        = "dvalue"
-        type        = 'text'
-        bind:value  = {key}
-    />
-
-    <!-- Min Max -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
-        <Input
-            label       = "Min Value"
-            placeholder = "Enter min value"
-            id          = "min-value"
-            name        = "min-value"
-            type        = 'number'
-            bind:value  = {key}
-        />
-
-        <Input
-            label       = "Max Value"
-            placeholder = "Enter max value"
-            id          = "max-value"
-            name        = "max-value"
-            type        = 'number'
-            bind:value  = {key}
-        />
-    </div>
-
-    <!-- Min Max Length -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
-        <Input
-            label       = "Min Length Value"
-            placeholder = "Enter min Length"
-            id          = "min-value-length"
-            name        = "min-value-length"
-            type        = 'number'
-            bind:value  = {key}
-        />
-
-        <Input
-            label       = "Max Length Value"
-            placeholder = "Enter max Length"
-            id          = "max-value-length"
-            name        = "max-value-length"
-            type        = 'number'
-            bind:value  = {key}
-        />
-    </div>
-
-    <!-- Min Max Date -->
-    <DatePicker label="Min Date" />
-    <DatePicker label="Max Date" />
-
-    <Input
-        label       = "Pattern"
-        placeholder = "Enter Pattern"
-        id          = "pattern"
-        name        = "pattern"
-        type        = 'text'
-        bind:value  = {key}
-    />
-
-    <Switch label="Required" />
-</form>
 </PanelMain>
 
 <PanelFooter>
-    <Modal id={'add-attribute'} type="attribute" title="Add Attribute" />
+    <Modal
+        id      = { 'add-attribute' }
+        type    = "attribute"
+        title   = "Add Attribute"
+    />
 
     <div class="flex items-center space-x-3">
         <button
             class="close-panel px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-gray-600 transition-colors duration-300 active:scale-[0.98] active:bg-gray-700"
-            onclick={() => {
-                clicked++;
-                console.log('🚀 ~ file: AttributeForm.svelte:112 ~ isPanelOpen:', clicked)
-            }}
+            onclick={ () => clicked++ }
         >
             Close
         </button>
@@ -120,8 +210,8 @@
         <button
             class="save-panel px-4 py-2 bg-neon-blue text-dark-blue rounded-md hover:bg-opacity-80 transition-colors duration-300"
             onclick={() => {
-                clicked++;
-                console.log('🚀 ~ file: AttributeForm.svelte:112 ~ isPanelOpen:', clicked)
+                // clicked++;
+                console.log('🚀 ~ file: AttributeForm.svelte:112 ~ isPanelOpen:', {...attribute})
             }}
         >
             Save
