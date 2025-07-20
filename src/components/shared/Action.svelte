ca1dd3aa810c35ab0f26daf54e4c01732ad0592e
kevinkeyssx <keyss1700@gmail.com>
2025-07-19 23:10:14 -0400

    Action component added

---
+++ src/components/shared/Action.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    import Panel        from "@/components/shared/panel/Panel.svelte";
+    import AlertDialog  from "@/components/shared/dialog/AlertDialog.svelte";
+
+
+    interface Props {
+        clicked     : number;
+        onDelete    : ( ) => void;
+        titleEdit   : string;
+        form        : Snippet;
+        type        : string;
+        data        : string;
+    }
+
+
+    const {
+        clicked = $bindable(),
+        onDelete,
+        titleEdit,
+        form,
+        type,
+        data
+    }: Props = $props();
+</script>
+
+<div class="flex gap-2 items-center">
+    <Panel
+        clicked     = { clicked }
+        title       = { titleEdit }
+        buttonText  = ""
+        buttonClass = ""
+        isEdit      = { true }
+    >
+        {@render form()}
+    </Panel>
+
+    <AlertDialog { onDelete } { type } { data } />
+</div>
