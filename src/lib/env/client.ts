08e5e20713636e36216dd2a9ad89954e9c0c524c
kevinkeyssx <keyss1700@gmail.com>
2025-07-27 03:00:12 -0400

    PUBLIC_FILE_UPLOAD_URL added

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -3,9 +3,10 @@ import { z } from 'zod';
 import { loadEnv, type Env } from './load-env';
 
 const clientEnvSchema = z.object({
-    PUBLIC_GUARDIANAPI_URL         : z.string().url(),
-    PUBLIC_GUARDIAN_AUTH_SOCIAL    : z.string(),
-    PUBLIC_GUARDIANAPI_GRAPHQL_URL : z.string().url()
+    PUBLIC_GUARDIANAPI_URL          : z.string().url(),
+    PUBLIC_GUARDIAN_AUTH_SOCIAL     : z.string(),
+    PUBLIC_GUARDIANAPI_GRAPHQL_URL  : z.string().url(),
+    PUBLIC_FILE_UPLOAD_URL          : z.string().url()
 });
 
 export type ClientEnv   = z.infer<typeof clientEnvSchema> & Env;
