60f3b0896ba7e1e45e38c39d4f425c61044ab7c2
kevinkeyssx <keyss1700@gmail.com>
2025-08-02 21:22:04 -0400

    Cancel Icon added

---
+++ src/icons/CancelIcon.svelte
@@ -0,0 +1,53 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="18"
+	height="18"
+	viewBox="0 0 24 24"
+>
+	<g
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+	>
+		<path
+			stroke-dasharray="64"
+			stroke-dashoffset="64"
+			d="M3 12c0 -4.97 4.03 -9 9 -9c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9Z"
+		>
+			<animate
+				fill="freeze"
+				attributeName="stroke-dashoffset"
+				dur="0.6s"
+				values="64;0"
+			/>
+		</path>
+		<path
+			stroke-dasharray="8"
+			stroke-dashoffset="8"
+			d="M9 9l6 6"
+		>
+			<animate
+				fill="freeze"
+				attributeName="stroke-dashoffset"
+				begin="0.6s"
+				dur="0.2s"
+				values="8;0"
+			/>
+		</path>
+		<path
+			stroke-dasharray="8"
+			stroke-dashoffset="8"
+			d="M15 9l-6 6"
+		>
+			<animate
+				fill="freeze"
+				attributeName="stroke-dashoffset"
+				begin="0.8s"
+				dur="0.2s"
+				values="8;0"
+			/>
+		</path>
+	</g>
+</svg>
