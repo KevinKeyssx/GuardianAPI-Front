01bdb8eb4d8a983d1426023a712ed24d9ba219c1
kevinkeyssx <keyss1700@gmail.com>
2025-08-02 21:21:44 -0400

    disabled & error  & onChange implemented

--- src/components/inputs/Switch.svelte
+++ src/components/inputs/Switch.svelte
@@ -7,6 +7,9 @@
         id?      : string;
         name?    : string;
         checked? : boolean | undefined;
+        disabled? : boolean | undefined;
+        error?   : string | undefined;
+        onChange? : ( value: boolean ) => void;
     }
 
 
@@ -15,6 +18,9 @@
         id       = "switch",
         name     = "switch",
         checked  = $bindable<boolean | undefined>(),
+        disabled = $bindable<boolean | undefined>(),
+        error    = $bindable<string | undefined>(),
+        onChange = $bindable<( value: boolean ) => void>(),
     }: Props = $props();
 
 
@@ -26,7 +32,8 @@
 <div class="flex items-center justify-between">
     <Label.Root
         for     = { id }
-        class   = "text-sm font-orbitron text-white">
+        class   = "text-sm font-orbitron text-white"
+    >
         { label }
     </Label.Root>
 
@@ -34,11 +41,16 @@
         bind:checked={checked}
         { id }
         { name }
-        onCheckedChange = {( value: boolean ) => checked = value }
+        { disabled }
+        onCheckedChange = {( value: boolean ) => {checked = value; onChange?.( value ); }}
         class           = "focus-visible:ring-neon-blue focus-visible:ring-offset-space-blue data-[state=checked]:bg-neon-blue data-[state=unchecked]:bg-space-blue data-[state=unchecked]:border-neon-blue/50 focus-visible:outline-none peer inline-flex h-[28px] min-h-[28px] w-[52px] shrink-0 cursor-pointer items-center rounded-full border border-neon-blue/50 px-[2px] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
     >
         <Switch.Thumb
             class="bg-white data-[state=checked]:bg-dark-blue pointer-events-none block size-[22px] shrink-0 rounded-full shadow-md transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
         />
     </Switch.Root>
+
+    {#if error}
+        <span class="text-red-400 text-sm">{ error }</span>
+    {/if}
 </div>
