136d1d5c72cc0d12cb7fe6b842fcbb615ba19f70
kevinkeyssx <keyss1700@gmail.com>
2025-08-04 00:44:01 -0400

    USERS_QUERY updated

--- src/lib/graphql/users/queries.ts
+++ src/lib/graphql/users/queries.ts
@@ -11,33 +11,6 @@ export const USER_ATTRIBUTES_QUERY = gql`
 
 
 export const USERS_QUERY = gql`
-    # query Users($page: Int, $each: Int, $field: String, $orderBy: String, $keys: [String!]) {
-    #     users {
-    #     # users( page: $page, each: $each, field: $field, orderBy: $orderBy, keys: $keys ) {
-    #         id
-    #         avatar
-    #         email
-    #         name
-    #         nickname
-    #         birthdate
-    #         phone
-    #         isActive
-    #         createdAt
-    #         updatedAt
-    #         roles {
-    #             name
-    #         }
-    #         # attributes (keys: $keys){
-    #         attributes {
-    #             type
-    #             key
-    #             value
-    #             required
-    #         }
-    #     }
-    # }
-
-    # query Users($keys: [String!]) {
     query Users {
         users {
             id
@@ -53,10 +26,10 @@ export const USERS_QUERY = gql`
             roles {
                 name
             }
-            # attributes(keys: $keys) {
             attributes {
                 key
                 value
+                valueId
                 type
                 required
                 max
