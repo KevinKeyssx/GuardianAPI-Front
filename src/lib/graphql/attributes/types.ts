c91cacc84e6c512e3d63307c7db5132ff9b500f1
kevinkeyssx <keyss1700@gmail.com>
2025-08-10 22:20:36 -0400

    BaseAttributeFields implemented

--- src/lib/graphql/attributes/types.ts
+++ src/lib/graphql/attributes/types.ts
@@ -1,23 +1,43 @@
 export type Type = 'NUMBER' | 'DECIMAL' | 'STRING' | 'DATETIME' | 'BOOLEAN' | 'JSON' | 'LIST' | 'UUID';
 
-export interface Attribute {
-    id?             : string;
-    key?            : string;
-    defaultValue?   : any;
-    type?           : Type;
-    required?       : boolean;
-    min?            : string;
-    max?            : string;
-    minLength?      : number;
-    maxLength?      : number;
-    minDate?        : string;
-    maxDate?        : string;
-    pattern?        : string;
-    isActive?       : boolean;
-    createdAt?      : string;
-    updatedAt?      : string;
+
+export interface BaseAttributeFields {
+	key?            : string;
+	defaultValue?   : any;
+	type?           : Type;
+	required?       : boolean;
+	min?            : string;
+	max?            : string;
+	minLength?      : number;
+	maxLength?      : number;
+	minDate?        : string;
+	maxDate?        : string;
+	pattern?        : string;
+	isActive?       : boolean;
 }
 
-export interface AttributesQuery {
-    userAttributes: Attribute[];
+export interface BaseEntity {
+	id?: string;
 }
+
+export interface BaseTimestamps {
+	createdAt? : string;
+	updatedAt? : string;
+}
+
+export interface Attribute extends BaseEntity, BaseAttributeFields, BaseTimestamps {}
+
+export interface AttributesQuery {
+	userAttributes  : Attribute[];
+}
+
+export interface CreateUserAttributeInput extends Omit<BaseAttributeFields, 'key' | 'type'> {
+	key             : string;
+	type            : Type;
+}
+
+export interface UpdateUserAttributeInput extends BaseEntity, BaseAttributeFields {
+	id              : string;
+}
+
+export interface DeleteUserAttributeInput extends Required<Pick<BaseEntity, 'id'>> {}
