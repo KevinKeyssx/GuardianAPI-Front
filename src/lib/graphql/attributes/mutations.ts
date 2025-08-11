59cfb671e99ce3341f2e8b7d65e2e0493857a964
kevinkeyssx <keyss1700@gmail.com>
2025-08-10 22:20:18 -0400

    CREATE_ATTRIBUTE_MUTATION & UPDATE_ATTRIBUTE_MUTATION & DELETE_ATTRIBUTE_MUTATION added

---
+++ src/lib/graphql/attributes/mutations.ts
@@ -0,0 +1,50 @@
+import { gql } from 'graphql-request';
+
+export const CREATE_ATTRIBUTE_MUTATION = gql`
+    mutation CreateUserAttribute( $createUserAttributeInput: CreateUserAttributeInput! ) {
+        createUserAttribute( createUserAttributeInput: $createUserAttributeInput ) {
+            id
+            key
+            defaultValue
+            type
+            required
+            min
+            max
+            minLength
+            maxLength
+            minDate
+            maxDate
+            pattern
+            isActive
+        }
+    }
+`;
+
+export const UPDATE_ATTRIBUTE_MUTATION = gql`
+    mutation UpdateUserAttribute($updateUserAttributeInput: UpdateUserAttributeInput!) {
+        updateUserAttribute(updateUserAttributeInput: $updateUserAttributeInput) {
+            id
+            key
+            defaultValue
+            isActive
+            type
+            min
+            max
+            minLength
+            maxLength
+            pattern
+            required
+            maxDate
+            minDate
+            valueId
+        }
+    }
+`;
+
+export const DELETE_ATTRIBUTE_MUTATION = gql`
+    mutation RemoveUserAttribute($removeUserAttributeId: ID!) {
+        removeUserAttribute(id: $removeUserAttributeId) {
+            id
+        }
+    }
+`;
