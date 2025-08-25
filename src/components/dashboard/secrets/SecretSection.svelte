adabee233cf77cc09e3199d2ddfad349ff738f52
kevinkeyssx <keyss1700@gmail.com>
2025-08-24 23:49:36 -0400

    Modal in update secret

--- src/components/dashboard/secrets/SecretSection.svelte
+++ src/components/dashboard/secrets/SecretSection.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+    // import { onMount } from 'svelte';
 
-    // import Panel            from "@/components/shared/Panel.svelte";
     import Table        from "@/components/shared/table/Table.svelte";
     import TableData    from "@/components/shared/table/TableData.svelte";
     import TableRow     from "@/components/shared/table/TableRow.svelte";
     import TableEmpty   from '@/components/shared/table/TableEmpty.svelte';
-    import Dialog       from '@/components/shared/dialog/dialog.svelte';
     import Modal        from '@/components/shared/Modal.svelte';
-    import AlertDialog  from '@/components/shared/dialog/AlertDialog.svelte';
     import SecretForm   from '@/components/dashboard/secrets/SecretForm.svelte';
-    // import SecretForm       from "@/components/dashboard/secrets/SecretForm.astro";
-    // import SearchIcon       from "@/icons/SearchIcon.astro";
+    import Filter       from '@/components/inputs/Filter.svelte';
+
     import { client }               from "@/lib/urql";
     import type { Secret }          from "@/lib/graphql/secrets/types";
     import { queryStore }           from '@urql/svelte';
@@ -27,37 +24,38 @@
     });
 
 
-    function refetchUsers() {
-        secretsResult.reexecute({ requestPolicy: 'network-only' });
-    }
+    // function refetchUsers() {
+    //     secretsResult.reexecute({ requestPolicy: 'network-only' });
+    // }
 
-    // Configurar intervalo para refrescar datos cada cierto tiempo (ej: cada 30 segundos)
-    let refreshInterval: ReturnType<typeof setInterval> | undefined;
+    // // Configurar intervalo para refrescar datos cada cierto tiempo (ej: cada 30 segundos)
+    // let refreshInterval: ReturnType<typeof setInterval> | undefined;
 
     // Función para iniciar el intervalo de actualización
-    function startRefreshInterval(intervalMs = 1000 * 60 * 30) { // 30 minutos por defecto
-        // Limpiar intervalo existente si hay alguno
-        if ( refreshInterval ) clearInterval( refreshInterval );
+    // function startRefreshInterval( intervalMs = 1000 * 60 * 30 ) { // 30 minutos por defecto
+    //     // Limpiar intervalo existente si hay alguno
+    //     if ( refreshInterval ) clearInterval( refreshInterval );
 
-        // Crear nuevo intervalo
-        refreshInterval = setInterval(() => {
-            console.log('Actualizando datos de usuarios...');
-            refetchUsers();
-        }, intervalMs );
-    }
+    //     // Crear nuevo intervalo
+    //     refreshInterval = setInterval(() => {
+    //         console.log( 'Actualizando datos de usuarios...' );
+    //         refetchUsers();
+    //     }, intervalMs );
+    // }
 
-    // Función para detener el intervalo
-    function stopRefreshInterval() {
-        if ( refreshInterval ) {
-            clearInterval( refreshInterval );
-            refreshInterval = undefined;
-        }
-    }
+    // // Función para detener el intervalo
+    // function stopRefreshInterval() {
+    //     if ( refreshInterval ) {
+    //         clearInterval( refreshInterval );
+    //         refreshInterval = undefined;
+    //     }
+    // }
 
-    onMount(() => {
-        startRefreshInterval();
-        return () => stopRefreshInterval();
-    });
+
+    // onMount(() => {
+    //     startRefreshInterval();
+    //     return () => stopRefreshInterval();
+    // });
 
 
     const columns = [
@@ -69,56 +67,42 @@
         { column: 'Updated',        showColumn: false },
         { column: 'Actions',        showColumn: true }
     ];
+
+
+    let searchTerm      = $state( "" );
+    let currentPage     = $state( 1 );
+
+
+    function handleSearchChange( value: string ): void {
+        searchTerm = value;
+        currentPage = 1;
+    }
 </script>
 
 <div class="animate-fade-in">
     <h1 class="text-2xl font-orbitron text-white mb-6">API Secrets</h1>
 
     <div class="flex justify-between items-center mb-4">
-        <div class="relative w-64">
-            <input
-                type            = "text"
-                placeholder     = "Search API keys..."
-                class           = "search-input w-full bg-dark-blue/30 border border-neon-blue/30 rounded-md py-2 pl-10 pr-4 text-white placeholder-gray-400 focus:outline-none focus:border-neon-blue/60"
-                data-section    = "secrets"
-            />
+        <Filter 
+            bind:value={ searchTerm }
+            placeholder = "Search users"
+            name        = "userSearch"
+            onChange    = { handleSearchChange }
+        />
 
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <!-- <SearchIcon /> -->
-                ...
-            </div>
-        </div>
-
-    <!-- <Dialog /> -->
-
-    <Modal
-        id      = { 'add-secret' }
-        type    = "secret"
-        title   = "Add Secret"
-    >
-        {#snippet form()}
-            <SecretForm
-                secret      = { {} as Secret }
-                clicked     = { 0 }
-                onSuccess   = {()=>{}}
-            />
-        {/snippet}
-    </Modal>
-
-    <!-- <AlertDialog onDelete ={() =>{}}>
-        {#snippet information()}
-        <p>Confirm</p>
-        {/snippet}
-    </AlertDialog> -->
-
-
-        <!-- <Panel client:only="svelte"
-            title           = "New Secret"
-            saveButtonText  = "Add"
-            buttonText      = "Generate New Key"
+        <Modal
+            id      = { 'add-secret' }
+            type    = "secret"
+            title   = "Add Secret"
         >
-            <SecretForm id="add-secret" secret={undefined} />
-        </Panel> -->
+            {#snippet form()}
+                <SecretForm
+                    secret      = { {} as Secret }
+                    clicked     = { 0 }
+                    onSuccess   = { () => {} }
+                />
+            {/snippet}
+        </Modal>
     </div>
 
     {#if $secretsResult.fetching}
@@ -130,20 +114,31 @@
             {#each $secretsResult.data.secret as secret}
                 <TableRow>
                     <TableData value={ secret.name } />
+
                     <TableData value={ secret.expiresAt } />
+
                     <TableData value={ secret.willExpireAt } />
+
                     <TableData value={ secret.isActive } />
+
                     <TableData value={ secret.createdAt } />
+
                     <TableData value={ secret.updatedAt } />
-                    <TableData size="text-sm font-medium" float={true}>
-                        Panel
-                        <!-- <Panel client:only="svelte"
-                            buttonText      = ""
-                            buttonClass     = ""
-                            isEdit          = { true }
+
+                    <TableData size="text-sm font-medium" float={ true }>
+                        <Modal
+                            id      = { 'add-secret' }
+                            type    = "secret"
+                            title   = "Add Secret"
                         >
-                            <SecretForm id={`id-${index}`} secret={secret} />
-                        </Panel> -->
+                            {#snippet form()}
+                                <SecretForm
+                                    secret      = { secret }
+                                    clicked     = { 0 }
+                                    onSuccess   = { () => {} }
+                                />
+                            {/snippet}
+                        </Modal>
                     </TableData>
                 </TableRow>
             {:else}
