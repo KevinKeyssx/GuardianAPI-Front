7aabcce9268819bbf45a8861d8e70f7eb6dbecaf
kevinkeyssx <keyss1700@gmail.com>
2025-08-30 22:00:16 -0400

    isModal & Modal component implemented

--- src/components/shared/Action.svelte
+++ src/components/shared/Action.svelte
@@ -3,6 +3,7 @@
 
     import Panel        from "@/components/shared/panel/Panel.svelte";
     import AlertDialog  from "@/components/shared/dialog/AlertDialog.svelte";
+    import Modal        from "@/components/shared/Modal.svelte";
 
 
     interface Props {
@@ -12,6 +13,7 @@
         form        : Snippet;
         type        : string;
         data        : string;
+        isModal?    : boolean;
     }
 
 
@@ -21,20 +23,34 @@
         titleEdit,
         form,
         type,
-        data
+        data,
+        isModal  = false
     }: Props = $props();
 </script>
 
 <div class="flex gap-2 items-center">
-    <Panel
-        clicked     = { clicked }
-        title       = { titleEdit }
-        buttonText  = ""
-        buttonClass = ""
-        isEdit      = { true }
-    >
-        {@render form()}
-    </Panel>
+    {#if isModal}
+        <Modal
+            id          = "edit-secret"
+            title       = { titleEdit }
+            buttonText  = ""
+            isEdit      = { true }
+        >
+            {#snippet form()}
+                {@render form!()}
+            {/snippet}
+        </Modal>
+    {:else}
+        <Panel
+            clicked     = { clicked }
+            title       = { titleEdit }
+            buttonText  = ""
+            buttonClass = ""
+            isEdit      = { true }
+        >
+            {@render form()}
+        </Panel>
+    {/if}
 
     <AlertDialog { onDelete } { type } { data } />
 </div>
