600bc34864859cd55db76459d0fbaba82abd9912
kevinkeyssx <keyss1700@gmail.com>
2025-08-30 22:00:47 -0400

    CreateIcon implemented

--- src/components/shared/panel/Panel.svelte
+++ src/components/shared/panel/Panel.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import EditIcon from "@/icons/EditIcon.svelte";
+    import CreateIcon   from "@/icons/CreateIcon.svelte";
+    import EditIcon     from "@/icons/EditIcon.svelte";
 
     let {
         title,
@@ -77,7 +78,7 @@
     {#if isEdit}
         <EditIcon />
     {:else}
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+        <CreateIcon />
     {/if}
 
     <span class="hidden sm:block">
