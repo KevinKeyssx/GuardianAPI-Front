bdc10beee5f233ad83be9b5d7b2affa55719f26d
kevinkeyssx <keyss1700@gmail.com>
2025-09-21 19:41:50 -0300

    GROUPS_QUERY & ROLES_WITH_PERMISSIONS_QUERY implemented

--- src/lib/graphql/roles/queries.ts
+++ src/lib/graphql/roles/queries.ts
@@ -30,3 +30,65 @@ export const PERMISSIONS_QUERY = gql`
         }
     }
 `;
+
+
+export const GROUPS_QUERY = gql`
+    query Groups($page: Int, $each: Int) {
+        groups(page: $page, each: $each) {
+            id
+            name
+            description
+            createdAt
+            updatedAt
+            isActive
+            roles {
+                id
+                name
+                description
+                isActive
+                permissions {
+                    id
+                    name
+                    description
+                    isActive
+                }
+            }
+            permissions {
+                id
+                name
+                description
+                isActive
+            }
+            users {
+                id
+                userId
+                user {
+                    id
+                    name
+                    email
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
+
+export const ROLES_WITH_PERMISSIONS_QUERY = gql`
+    query RolesWithPermissions($page: Int, $each: Int) {
+        roles(page: $page, each: $each) {
+            id
+            name
+            description
+            createdAt
+            updatedAt
+            isActive
+            permissions {
+                id
+                name
+                description
+                isActive
+            }
+        }
+    }
+`;
