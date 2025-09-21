37fa8a577b3c38741253388274044c4457d2af6d
kevinkeyssx <keyss1700@gmail.com>
2025-09-21 19:42:12 -0300

    Group & GroupUser implemented

--- src/lib/graphql/roles/types.ts
+++ src/lib/graphql/roles/types.ts
@@ -6,9 +6,58 @@ export type Role = {
     createdAt?      : string;
     updatedAt?      : string;
     isActive?       : boolean;
+    permissions?    : Permission[];
+}
+
+
+export type Permission = {
+    id?             : string;
+    name?           : string;
+    description?    : string;
+    userId?         : string;
+    createdAt?      : string;
+    updatedAt?      : string;
+    isActive?       : boolean;
+}
+
+
+export type Group = {
+    id?             : string;
+    name?           : string;
+    description?    : string;
+    userId?         : string;
+    createdAt?      : string;
+    updatedAt?      : string;
+    isActive?       : boolean;
+    roles?          : Role[];
+    permissions?    : Permission[];
+    users?          : GroupUser[];
+}
+
+
+export type GroupUser = {
+    id?             : string;
+    userId?         : string;
+    groupId?        : string;
+    user?           : {
+        id?         : string;
+        name?       : string;
+        email?      : string;
+        avatar?     : string;
+    };
 }
 
 
 export type RolesQuery = {
     roles: Role[];
+}
+
+
+export type PermissionsQuery = {
+    permissions: Permission[];
+}
+
+
+export type GroupsQuery = {
+    groups: Group[];
 }
