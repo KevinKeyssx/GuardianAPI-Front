bafa4a37e58766d895f8b406caaf3d12d9410d90
kevinkeyssx <keyss1700@gmail.com>
2025-09-21 13:43:05 -0300

    field & orderBy commented

--- src/lib/graphql/roles/queries.ts
+++ src/lib/graphql/roles/queries.ts
@@ -1,26 +1,32 @@
 import { gql } from 'graphql-request';
 
 export const ROLES_QUERY = gql`
-    query Roles($page: Int, $each: Int, $field: String, $orderBy: String) {
-        roles(page: $page, each: $each, field: $field, orderBy: $orderBy) {
+    query Roles($page: Int, $each: Int) {
+    # query Roles($page: Int, $each: Int, $field: String, $orderBy: String) {
+        # roles(page: $page, each: $each, field: $field, orderBy: $orderBy) {
+        roles(page: $page, each: $each) {
             id
             name
             description
             createdAt
             updatedAt
+            isActive
         }
     }
 `;
 
 
 export const PERMISSIONS_QUERY = gql`
-    query Permissions($page: Int, $each: Int, $field: String, $orderBy: String) {
-        permissions(page: $page, each: $each, field: $field, orderBy: $orderBy) {
+    query Permissions($page: Int, $each: Int) {
+    # query Permissions($page: Int, $each: Int, $field: String, $orderBy: String) {
+        # permissions(page: $page, each: $each, field: $field, orderBy: $orderBy) {
+        permissions(page: $page, each: $each) {
             id
             name
             description
             createdAt
             updatedAt
+            isActive
         }
     }
 `;
